41a93188a8268c29a38f11ef52986a9b07f703b3
AlyciaHathaway <Alyciahathaways@gmail.com>
2017-10-17 10:16:13 +0800

    update PAT 4-2

--- C语言/第4周：循环控制/PAT.c
+++ C语言/第4周：循环控制/PAT.c
@@ -31,3 +31,66 @@ int main() {
 	
 	return 0;
 }
+
+int main() {
+	int x;
+	int mask = 1;
+	scanf("%d", &x);
+	
+	if (x<0) {
+		x *= -1;
+		printf("fu ");
+	}
+	
+	int t = x;
+	while (t > 9) {
+		t /= 10;
+		mask *= 10;
+	}
+
+	do {
+		int digital = x / mask;
+//		printf("%d ", digital);
+		switch (digital) {
+			case 0:
+				printf("ling");
+				break;
+			case 1:
+				printf("yi");
+				break;
+			case 2:
+				printf("er");
+				break;
+			case 3:
+				printf("san");
+				break;
+			case 4:
+				printf("si");
+				break;
+			case 5:
+				printf("wu");
+				break;
+			case 6:
+				printf("liu");
+				break;
+			case 7:
+				printf("qi");
+				break;
+			case 8:
+				printf("ba");
+				break;
+			case 9:
+				printf("jiu");
+				break;
+			default:
+				printf("error");
+		}
+		if (mask > 9) {
+			printf(" ");
+		}
+		x %= mask;
+		mask /= 10;
+	} while (mask > 0);
+	
+	return 0;
+} 
